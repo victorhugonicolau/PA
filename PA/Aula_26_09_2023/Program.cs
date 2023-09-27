4b303121726b6931bb68e3ed107290299072c94c
Aluno <aluno@etec.sp.gov.br>
2023-09-26 22:48:41 -0300

    Atividades Aula_26_09_2023

--- PA/Aula_26_09_2023/Program.cs
+++ PA/Aula_26_09_2023/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace teste
+namespace Aula_26_09_2023
 {
     class Program
     {
         static void Main(string[] args)
         {
-            float n1, n2, n3, n4, media;
+            float n1, n2, n3, n4, media, percFrequencia;
 
             Console.Write("Digite a primeira nota: ");
             n1 = float.Parse(Console.ReadLine());
@@ -20,29 +20,30 @@ namespace teste
             Console.Write("Digite a quarta nota: ");
             n4 = float.Parse(Console.ReadLine());
 
+            Console.Write("\nInforme o percentual de frequência: ");
+            percFrequencia = float.Parse(Console.ReadLine());
+
             media = (n1 + n2 + n3 + n4) / 4;
 
-            if (media >= 0 && media < 5)
+            if (media < 5 && percFrequencia <= 40 ) 
             {
-                Console.WriteLine($"\n Sua média é {media}, reprovado");
+                Console.WriteLine("Aluno Reprovadon");
             }
 
-            else if (media >= 5 && media < 7)
+            else if (media < 7 && percFrequencia <= 60)
             {
-                Console.WriteLine($"\n Sua média é {media}, recuperação");
+                Console.WriteLine("Aluno em recuperação");
             }
 
-            else if (media >= 7 && media <= 10)
+            else if (media <= 10 && percFrequencia > 60)
             {
-                Console.WriteLine($"\n Sua média é {media}, aprovado");
+                Console.WriteLine("Aluno Aprovado");
             }
 
             else
             {
-                Console.WriteLine($"\nSua média é {media}." +
-                    $"\nA Média deve estar entre 0 e 10");
+                Console.WriteLine("Será analisado pela secretaria acadêmica");
             }
-
         }
     }
 }
