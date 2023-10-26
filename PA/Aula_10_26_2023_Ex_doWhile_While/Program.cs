ae7c51c7bebaf69480b070fc7138c58afbe77ce5
Aluno <aluno@etec.sp.gov.br>
2023-10-26 20:48:58 -0300

    Aula_26_10_2023_Atividades_com_whileDoWhile

---
+++ PA/Aula_10_26_2023_Ex_doWhile_While/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aula_10_26_2023_Ex_doWhile_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorFinanciado, valorParcela;
+            int parcela, contador = 1;
+
+            Console.Write("Informe o valor financiado: ");
+            valorFinanciado = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe o número de parcelas: ");
+            parcela = int.Parse(Console.ReadLine());
+
+            valorParcela = valorFinanciado / parcela;
+
+            DateTime data = DateTime.Now.Date; // DateTime armazena uma data e um horário-
+
+            while (contador <= parcela)
+            {
+                //Console.WriteLine($"Parcela n° {contador}, valor = {valorFinanciado / parcela}");
+                //Console.WriteLine($"{contador}ª parcela: {valorFinanciado / parcela:0.00}");
+                Console.WriteLine($"{contador}ª parcela ({data}): {valorParcela}");
+                contador++;
+                data = data.AddMonths(1);
+            }
+
+            Console.WriteLine("ta pago"); // criar váriavel para realizar a conta no laço de repetição consome menos memória
+        }                                 // Pois a conta será realizada apenas uma vez, já colocando no laço a conta será feita várias vezes    
+    }
+}
---
+++ PA/Aula_10_26_2023_doWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula_10_26_2023_doWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+
+            do // while faz o teste no inicio / do while faz o texte no final 
+            {
+                Console.WriteLine($"2 x {contador} = {2 * contador}");
+                contador++;
+            } while (contador <= 10);
+
+            Console.WriteLine("Fim da tabuada");
+        }
+    }
+}
