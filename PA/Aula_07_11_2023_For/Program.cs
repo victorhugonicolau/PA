d910fd6701640f781c634b968b757356046d41a8
Aluno <aluno@etec.sp.gov.br>
2023-11-07 22:47:55 -0300

    Aula 07_11_2023 - atividades com o For e novo operador "%" de verificar o resto de divisão.

---
+++ PA/Aula_07_11_2023_For/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula_07_11_2023_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            exemplo2();
+        }
+
+        static void exemplo1()
+        {
+            for (int contador = 1; contador <= 10; contador++) // já vem pré feito os atributos para o laço de repetição 
+            {
+                Console.WriteLine($"2 x {contador} = {2 * contador}");
+            }
+
+            Console.WriteLine("fim da tabuada");
+        } // for exemplo com tabuada do 2
+
+        static void exemplo2()
+
+        {
+            int fator;
+
+            Console.Write("Digite o fator: ");
+            fator = int.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador <= 10; contador++) 
+            {
+                Console.WriteLine($"{fator} x {contador} = {fator * contador}");
+            }
+        }
+    }
+}
---
+++ PA/Aula_11_07_2023_Par_Impar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula_11_07_2023_Par_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+
+            Console.Write("Digite o número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2== 0)
+            {
+                Console.Write("O número é par");
+            }
+
+            else
+            {
+                Console.WriteLine("O número é impar");
+            }
+        }
+    }
+}
