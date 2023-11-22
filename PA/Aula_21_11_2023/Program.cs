8203db2599c69c413fae893fef1367f3f6d373b5
Aluno <aluno@etec.sp.gov.br>
2023-11-21 22:44:59 -0300

    Aula 21 11 2023

--- PA/Aula_21_11_2023/Program.cs
+++ PA/Aula_21_11_2023/Program.cs
@@ -6,15 +6,34 @@ namespace Aula_21_11_2023
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ex_1();
         }
 
         static void ex_1()
-        {
-            for (int i = 0; i < length; i++)
-            {
 
+        {
+            float alturaGeral;
+            float alturaMaior = 0, alturaMenor = 0;
+
+            for (int contador = 1; contador <= 15; contador++)
+            {
+                Console.Write("Informe a altura: ");
+                alturaGeral = float.Parse(Console.ReadLine());
+
+                if (alturaGeral >= alturaMaior)
+                {
+                    alturaMaior = alturaGeral;
+                }
+
+                else if (alturaGeral <= alturaMenor || alturaMenor == 0)
+                {
+                    alturaMenor = alturaGeral;
+                }
             }
+
+            Console.WriteLine($"Maior altura: {alturaMaior}");
+            Console.WriteLine($"Menor altura: {alturaMenor}");
+
         }
     }
 }
